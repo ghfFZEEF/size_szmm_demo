b0a2375dd255b76af1fef3e4fa2465178391da55
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-20 19:27:11 +0300

    Added main page processing

--- szmm/views.py
+++ szmm/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context: dict[str, str] = {
+        'page_title': 'Главная страница'
+    }
+    return render(request, 'szmm/index.html', context)
