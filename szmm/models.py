1a8faa8d697ae01c8cc9765896e005f427d30e1a
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-27 00:09:14 +0300

    Changed the name of the class

--- szmm/models.py
+++ szmm/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Goods(models.Model):
+class GoodsTypes(models.Model):
     name: models.CharField = models.CharField(max_length=30)
     image: models.IntegerField = models.ImageField()
     description: models.CharField = models.CharField(max_length=200)
