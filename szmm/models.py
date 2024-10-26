9884f1caad207a5c589d56e974d1810b2ad990f2
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-27 00:11:23 +0300

    changed the description to a link

--- szmm/models.py
+++ szmm/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class GoodsTypes(models.Model):
     name: models.CharField = models.CharField(max_length=30)
     image: models.IntegerField = models.ImageField()
-    description: models.CharField = models.CharField(max_length=200)
+    link: models.CharField = models.CharField(max_length=200)
