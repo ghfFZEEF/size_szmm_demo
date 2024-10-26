09cef27b28dc1c2a6de8d97826e4a76ab95552b8
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-27 00:56:32 +0300

    Reworked the index function in views.py

--- szmm/views.py
+++ szmm/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from szmm.models import GoodsTypes
+
+
 def index(request: HttpRequest) -> HttpResponse:
-    context: dict[str, str] = {
-        'page_title': 'Главная страница'
+    context: dict[str, str | list] = {
+        "page_title": "Главная страница",
+        "data": GoodsTypes.objects.all(),
     }
-    return render(request, 'szmm/index.html', context)
+
+    return render(request, "szmm/index.html", context)
