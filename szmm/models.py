55b8b1557a4d7d2551898be40f41f07a686dccff
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-27 00:16:34 +0300

    Изменил хранение картинок

--- szmm/models.py
+++ szmm/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class GoodsTypes(models.Model):
     name: models.CharField = models.CharField(max_length=30)
-    image: models.IntegerField = models.ImageField()
+    image_link: models.CharField = models.CharField(max_length=200)
     link: models.CharField = models.CharField(max_length=200)
