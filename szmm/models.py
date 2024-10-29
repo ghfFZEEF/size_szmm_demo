17b7f84d254be30f733915e55d60313dce8e02d8
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-29 23:18:44 +0300

    Added picture processing

--- szmm/models.py
+++ szmm/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class GoodsTypes(models.Model):
     name: models.CharField = models.CharField(max_length=30)
-    image_link: models.CharField = models.CharField(max_length=200)
+    image_link: models.ImageField = models.ImageField(null=True, blank=True, upload_to="goods_types_images/")
     link: models.CharField = models.CharField(max_length=200)
