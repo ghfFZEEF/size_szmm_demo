de74ded19c822f58d1bc086c1ce7f62da513dc8c
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-31 20:17:47 +0300

    Changed the Pulsator model and conducted a migration

--- szmm/models.py
+++ szmm/models.py
@@ -9,5 +9,9 @@ class GoodsType(models.Model):
 
 class Pulsator(models.Model):
     name: models.CharField = models.CharField(max_length=30)
-    image_link: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
     description: models.CharField = models.CharField(max_length=200)
+    image_link_1: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
+    image_link_2: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
+    image_link_3: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
+    image_link_4: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
+    image_link_5: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
