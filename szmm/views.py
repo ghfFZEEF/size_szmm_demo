f530039757e4396a54e9a57a20976adc8dc04436
Ruslan <rusya.kirichenko.08@mail.ru>
2024-10-31 22:40:17 +0300

    Fixed errors in the navigation system

--- szmm/views.py
+++ szmm/views.py
@@ -9,6 +9,7 @@ def index(request: HttpRequest) -> HttpResponse:
     context: dict[str, str | list] = {
         "page_title": "Главная страница",
         "data": models.GoodsType.objects.all(),
+        "index": True
     }
 
     return render(request, "szmm/index.html", context)
@@ -16,7 +17,7 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def goods(request: HttpRequest, goods_link: str) -> HttpResponse:
     context: dict[str, str | list] = {
-        "pulsators": {"page_title": "Доилки", "data": models.Pulsator.objects.all()}
+        "pulsators": {"page_title": "Доилки", "data": models.Pulsator.objects.all(), "index": False}
     }
 
     return render(request, "szmm/goods.html", context[goods_link])
