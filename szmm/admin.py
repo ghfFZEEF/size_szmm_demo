cb6528411bfe159f6ee3040c8beb5258d08cae9b
Ruslan <rusya.kirichenko.08@mail.ru>
2024-11-02 23:29:41 +0300

    Added GrainCrushers and GrassChoppers models

--- szmm/admin.py
+++ szmm/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import GoodsType, Pulsator, QrCode
+from .models import GoodsType, Pulsator, GrainCrushers, GrassChoppers, QrCode
 
 admin.site.register(GoodsType)
 admin.site.register(Pulsator)
+admin.site.register(GrainCrushers)
+admin.site.register(GrassChoppers)
 admin.site.register(QrCode)
--- szmm/models.py
+++ szmm/models.py
@@ -20,12 +20,32 @@ class GoodsType(models.Model):
 
 class Pulsator(models.Model):
     name: models.CharField = models.CharField(max_length=30)
-    description: models.CharField = models.CharField(max_length=200)
+    description: models.CharField = models.CharField(max_length=1000)
     image_1: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
     image_2: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
     image_3: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
     image_4: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
     image_5: models.ImageField = models.ImageField(null=True, blank=True, upload_to="pulsator_images/")
+
+
+class GrainCrushers(models.Model):
+    name: models.CharField = models.CharField(max_length=30)
+    description: models.CharField = models.CharField(max_length=1000)
+    image_1: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grain_crushers_images/")
+    image_2: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grain_crushers_images/")
+    image_3: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grain_crushers_images/")
+    image_4: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grain_crushers_images/")
+    image_5: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grain_crushers_images/")
+
+
+class GrassChoppers(models.Model):
+    name: models.CharField = models.CharField(max_length=30)
+    description: models.CharField = models.CharField(max_length=1000)
+    image_1: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grass_choppers_images/")
+    image_2: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grass_choppers_images/")
+    image_3: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grass_choppers_images/")
+    image_4: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grass_choppers_images/")
+    image_5: models.ImageField = models.ImageField(null=True, blank=True, upload_to="grass_choppers_images/")
 
 
 class QrCode(models.Model):
